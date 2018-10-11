73679afc30f6c93ca149db425960bb75077c803a
Mth-bs-park <mth.bs.park@gmail.com>
2018-10-12 01:04:19 +0900

    add component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+class Header extends Component {
+  render() {
+
+    return (
+      <div className='header'>
+        <div className='text-container'>
+          this is header  
+        </div>
+        <div className='right-menu-container'>
+          <ul>
+            <li>menu1</li>
+            <li>menu2</li>
+            <li>menu3</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <div className="container">
+          <Counter/>
+        </div>
       </div>
     );
   }
 }
+
+const Counter = (props) => (
+  <div className='counter-container'>
+    {props.num ? props.num : 0}
+  </div>
+);
 
 export default App;
