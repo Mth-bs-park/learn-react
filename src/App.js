9d23e3cf1eaa3ce306f358a000806c957393351a
Mth-bs-park <mth.bs.park@gmail.com>
2018-10-12 18:27:36 +0900

    add redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import logo from './logo.svg';
 import './App.scss';
 
@@ -23,22 +24,64 @@ class Header extends Component {
 }
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <Header/>
         <div className="container">
+          <button onClick={this.props.increase}>increase</button>
+          <button onClick={this.props.decrease}>decrease</button>
           <Counter/>
+          <Counter2/>
         </div>
       </div>
     );
   }
 }
 
-const Counter = (props) => (
+const increase = () => ({type: 'increase'});
+
+const decrease = () => ({type: 'decrease'});
+
+App = connect(
+  (state) => ({}),
+  (dispatch) => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease()),
+  })
+)(App);
+
+const Counter = connect(
+  (state) => ({num: state.counter.num, number: state.counter.number}),
+  (dispatch) => ({
+
+  })
+)((props) => (
   <div className='counter-container'>
-    {props.num ? props.num : 0}
+    {props.num}/{props.number.value}
   </div>
-);
+));
+
+class Counter2 extends Component {
+
+  componentWillUpdate(nextProps, nextState){
+    console.log(nextProps);
+    nextProps.number.value += 1;
+  }
+
+  render() {
+    return (
+      <div className='counter-container'>
+        {this.props.num}/{this.props.number.value}
+      </div>
+    )
+  }
+}
+
+Counter2 = connect(
+  (state) => ({num: state.counter.num, number: state.counter.number}),
+  (dispatch) => ({})
+)(Counter2)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state = {num: 0, number: {value: 0}}, action) => {
+
+  switch (action.type) {
+    case 'increase':
+      return {
+        ...state,
+        num: state.num + 1,
+        number: {
+          value: state.number.value + 1,
+        }
+      };
+    case 'decrease':
+      return {
+        ...state,
+        num: state.num - 1,
+        number: {
+          value: state.number.value - 1,
+        }
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  combineReducers({
+    counter: reducer
+  })
+);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
